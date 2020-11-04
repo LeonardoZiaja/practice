7a66d0589f635ca206bc1b06ebee4d4fc3a6cc3b
CarstenSt <cstukenb@smail.uni-koeln.de>
2020-11-04 11:00:22 +0100

    change in HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,5 +6,7 @@ public class HelloWorld
              // Ausgabe Hello World!
              System.out.println("Jahre an der Uni: 3 (drei)");
              System.out.println("Leonardo Maximilian Ziaja");
+
+            System.out.println("Aenderung von Carsten Hurra!"); 
        }
 }
