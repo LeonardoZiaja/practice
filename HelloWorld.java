98ccd53699a8c08ec62d3598475795e0949ddee6
LeonardoZiaja <lziaja@smail.uni-koeln.de>
2020-11-04 10:47:03 +0100

    Update: Add Years of Enrollment

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld
        public static void main (String[] args)
        {
              // Ausgabe Hello World!
-             System.out.println("Hello World!");
+             System.out.println("Jahre an der Uni: 3 (drei)");
        }
 }
