4204f83d0164095ac5b6c9e756835ea03e33a9f9
CarstenSt <cstukenb@smail.uni-koeln.de>
2020-11-04 11:28:50 +0100

    feat: calculate years of enrollment

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,30 @@
-public class HelloWorld 
+import java.time.LocalDateTime;
+import java.time.Duration;
+
+public class HelloWorld
 {
- 
+
+	private final static LocalDateTime start_carsten = LocalDateTime.of(2017,10,1,0,0);
+
        public static void main (String[] args)
        {
              // Ausgabe Hello World!
              System.out.println("Jahre an der Uni: 3 (drei)");
              System.out.println("Leonardo Maximilian Ziaja");
 
-            System.out.println("Aenderung von Carsten Hurra!"); 
+
+		//Berechnung Carsten
+		LocalDateTime now = LocalDateTime.now(); 
+
+		System.out.println("Carsten hat die Uni am "+start_carsten.toString()+" gestartet"); 
+
+		Duration duration = Duration.between(start_carsten, now); 
+
+		long days = duration.toDays(); 
+
+		long years = days/365; 
+
+		System.out.println("Carsten ist seit "+days+" Tagen und "+years+" Jahren eingeschrieben.");
+
        }
 }
